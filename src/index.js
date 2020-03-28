4dba9533172e604fc8efb383bfc475551b4bad33
Cesar Garzon <ceagarzonpu@unal.edu.co>
2020-03-28 13:35:10 -0500

    Inicio de proyecto

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
+// const element = document.createElement('h1');
+
+// element.innerText = 'Hello React';
+
+// const container = document.getElementById('root');
+
+// container.appendChild(element);
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const user = {
+  firtName: "Cesar",
+  lastName: 'Garzón',
+  avatar: 'https://upload.wikimedia.org/wikipedia/commons/8/87/Avatar_poe84it.png'
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function getName (user){
+  return `${user.firtName} ${user.lastName}`
+}
+
+function getGreeting(user){
+  if(user){
+   return <h1>Hello {getName(user)}</h1>
+  }
+  return <h1>Hello Stranger</h1>
+}
+
+const name = 'Cesar';
+const element = (
+  <div>
+    <h1>Hello {getName(user)}</h1>
+    <img src={user.avatar}/>
+  </div>
+); 
+
+
+const container = document.getElementById('root');
+
+ReactDOM.render(element, container); 
